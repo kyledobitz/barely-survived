c8dd8306fda4bf8d6102b9f9e27324fef1c7e703
Kyle Dobitz <kyledobitz@gmail.com>
2015-10-17 20:38:39 -0700

    Cool Skybox, metal texture and stand in rooms rotation

---
+++ Unity Project/Barely Survived/Assets/Scripts/GameScripts/RotatingFrame.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingFrame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public float period = 30.0F;
+	public Vector3 axis = Vector3.up + Vector3.right;
+	void Update() {
+		float angle = 360 * Time.deltaTime / period;
+		transform.rotation = transform.rotation * Quaternion.AngleAxis (angle, axis);
+	}
+}
