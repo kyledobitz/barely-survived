c44690b4f2e93b5104c3a5217970319a65e718e5
Kyle Dobitz <kyledobitz@gmail.com>
2015-10-17 23:34:00 -0700

    Added rudimentary Meanders script

---
+++ Unity Project/Barely Survived/Assets/Scripts/GameScripts/Meanders.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Meanders : MonoBehaviour {
+
+	public Collider currentRoom;
+
+	public float speed = 1f;
+
+	private Vector3 destination;
+	private Vector3 velocity;
+	private Vector3 heading;
+
+	public float standVsMeanderPercent=0.5f;
+	public float minDelayTime=1.0f;
+	public float maxDelayTime=2.0f;
+	private float nextDecisionTime=0f;
+	private State state = State.STANDING;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (state) {
+		case State.STANDING:
+			if (Time.time > nextDecisionTime) {
+				nextDecisionTime = getNextDecisionTime ();
+				state = DecideNextState ();
+			}
+			velocity = Vector3.zero;
+			break;
+		case State.MEANDERING:
+			if (Time.time > nextDecisionTime || IsAtDestination ()) {
+				nextDecisionTime = getNextDecisionTime ();
+				state = DecideNextState ();
+			}
+			break;
+		}
+	}
+	
+	bool IsAtDestination ()
+	{
+		return (transform.position - destination).magnitude < 0.2f;
+	}
+	
+	void ChooseMeanderDestination ()
+	{
+		var minX = currentRoom.bounds.min.x;
+		var minZ = currentRoom.bounds.min.z;
+		var maxX = currentRoom.bounds.max.x;
+		var maxZ = currentRoom.bounds.max.z;
+		destination = new Vector3 (Random.Range (minX, maxX), transform.position.y, Random.Range (minZ, maxZ));
+	}
+		
+	float getNextDecisionTime ()
+	{
+		return Time.time + Random.Range (minDelayTime, maxDelayTime);
+	}
+
+	State DecideNextState ()
+	{
+		if (Random.value < standVsMeanderPercent) {
+			return State.STANDING;
+		} else {
+			ChooseMeanderDestination ();
+			return State.MEANDERING;
+		}
+	}
+	
+	void Move ()
+	{
+		if ((transform.position - destination).magnitude < 0.1f) 
+			return;
+		heading = (destination - transform.position).normalized;
+		transform.forward = heading;
+		velocity = heading * speed;
+		transform.position += Time.deltaTime * velocity;
+	}
+	
+	public enum State
+	{
+		MEANDERING,
+		STANDING,
+		GOING_TO_DOOR
+	}
+}
