696eb3c1593d8e9bfe10616fc344ba9ef7a88077
phantomfreq <topherpirkl@gmail.com>
2015-10-25 16:26:22 -0500

    Main Scene now has theme music assuming you can hear anything

--- Unity Project/Barely Survived/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Unity Project/Barely Survived/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -34,20 +34,6 @@ namespace AK
 
     } // namespace SWITCHES
 
-    namespace GAME_PARAMETERS
-    {
-        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
-        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
-        static const AkUniqueID SS_AIR_FURY = 1029930033U;
-        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
-        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
-        static const AkUniqueID SS_AIR_RPM = 822163944U;
-        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
-        static const AkUniqueID SS_AIR_STORM = 3715662592U;
-        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
-        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
-    } // namespace GAME_PARAMETERS
-
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
@@ -58,6 +44,7 @@ namespace AK
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MUSIC = 3991942870U;
     } // namespace BUSSES
 
 }// namespace AK
