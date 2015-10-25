f184a8552fdc29e4a16277dca82448ca0b6ef5cf
Kyle Dobitz <kyledobitz@gmail.com>
2015-10-25 14:58:18 -0700

    Rudimentary assignment working

--- Unity Project/Barely Survived/Assets/Scripts/GameScripts/People/PersonHealth.cs
+++ Unity Project/Barely Survived/Assets/Scripts/GameScripts/People/PersonHealth.cs
@@ -1,25 +1,43 @@
-﻿using UnityEngine;
+﻿using Mono.Xml.Xsl;
 using System.Collections;
+using UnityEngine;
 
 public class PersonHealth : MonoBehaviour {
 
     public float maxHealth = 100f;
     public float health;
     public float corpseThickness = 0.2f;
+    public float lifeSupportEffectRate = 5f;
+    private LifeSupportEngine _lifeSupportEngine;
 
 	// Use this for initialization
 	void Start () {
 		health = maxHealth;
+        _lifeSupportEngine = FindObjectOfType<LifeSupportEngine>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		health += - Time.deltaTime * 10;
+        ApplyLifeSupportLevel();
         if(health <= 0){
             LeaveCorpse();
 			AkSoundEngine.PostEvent("Test", gameObject);
         }
+        if(health > maxHealth) {
+            health = maxHealth;
+        }
 	}
+
+    void ApplyLifeSupportLevel(){
+        float healthPercentage = 100f * health / maxHealth;
+        float supportVsHealthDiff = _lifeSupportEngine.lifeSupport - healthPercentage;
+        if(_lifeSupportEngine.lifeSupport < healthPercentage){
+            health -= lifeSupportEffectRate * Time.deltaTime;
+        }else {
+            health += lifeSupportEffectRate * Time.deltaTime;
+        }
+
+    }
 
     void LeaveCorpse(){
         var mesh = transform.Find("Mesh");
--- Unity Project/Barely Survived/Assets/Scripts/GameScripts/Ship/LifeSupportRoom.cs
+++ Unity Project/Barely Survived/Assets/Scripts/GameScripts/Ship/LifeSupportRoom.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class LifeSupportRoom : MonoBehaviour {
     public float lifeSupportProduction = 0f;
     public float lifeSupportProductionRate = 10f;
+    public float medsConsumptionRate = 1f;
+
     private Room _room;
+    private GameManager _gameManager;
 
 	// Use this for initialization
 	void Start () {
+        _gameManager = FindObjectOfType<GameManager>();
         _room = GetComponent<Room>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        lifeSupportProduction = (float) _room.members.Count * lifeSupportProductionRate;
+        float medsConsumption = (float) _gameManager.livingPeople.Count * medsConsumptionRate;
+        lifeSupportProduction = (float) _room.members.Count *lifeSupportProductionRate - medsConsumption;
 	}
 }
